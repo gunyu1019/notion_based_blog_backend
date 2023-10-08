4b5403cfea35dcbb90127d14dbee06959f612b24
gunyu1019 <gunyu1019@gmail.com>
2023-10-09 04:35:51 +0900

    [Feat] Add multi_select_property

---
+++ models/notion/property/multi_select_property.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, PrivateAttr
+from modules.notion import colors
+from utils.find_enum import find_enum
+
+
+class MultiSelectProperty(BaseModel):
+    id: str
+    name: str
+    _color: str = PrivateAttr
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._color = data.get('color')
+
+    @property
+    def color(self) -> colors.Colors:
+        return find_enum(colors.Colors, self._color)
