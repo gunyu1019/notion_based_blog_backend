ec48ee578e5bb570711195ce7082c817bd62caa8
gunyu1019 <gunyu1019@gmail.com>
2023-10-13 15:04:13 +0900

    [Feat] Use colorable

--- models/notion/property/multi_select_property.py
+++ models/notion/property/multi_select_property.py
@@ -1,17 +1,11 @@
 from pydantic import BaseModel, PrivateAttr
-from modules.notion import colors
-from utils.find_enum import find_enum
+from ..colorable import Colorable
 
 
-class MultiSelectProperty(BaseModel):
+class MultiSelectProperty(Colorable):
     id: str
     name: str
-    _color: str = PrivateAttr
 
     def __init__(self, **data):
         super().__init__(**data)
         self._color = data.get("color")
-
-    @property
-    def color(self) -> colors.Colors:
-        return find_enum(colors.Colors, self._color)
