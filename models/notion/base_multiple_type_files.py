0f9eb4ebb35cd7fbef4b64dcd6b5be38fd3c6db7
gunyu1019 <gunyu1019@gmail.com>
2023-11-06 02:13:23 +0900

    [Fix] Wrong ModelPrivateAttr

--- models/notion/base_multiple_type_files.py
+++ models/notion/base_multiple_type_files.py
@@ -1,16 +1,22 @@
 from abc import ABC
 from pydantic import BaseModel, PrivateAttr, field_validator
+from pydantic.fields import ModelPrivateAttr
 from .emoji import Emoji
 from .file import File
 
 
 class BaseMultipleTypeFile(BaseModel, ABC):
-    _multiple_type_file_key: str | list[str] = PrivateAttr
+    _multiple_type_file_key: ModelPrivateAttr = PrivateAttr([""])
 
     @classmethod
     @field_validator(
-        *([_multiple_type_file_key] if isinstance(_multiple_type_file_key, str) else _multiple_type_file_key),
-        mode="before")
+        *(
+                [_multiple_type_file_key.default]
+                if isinstance(_multiple_type_file_key.default, str)
+                else _multiple_type_file_key.default
+        ),
+        mode="before"
+    )
     def convert_multiple_type_emoji_or_file(cls, v: dict[str, str] | None):
         obj_type = v.get("type")
         if obj_type == "emoji":
