837e304c36b577d8a92404dcd8c13e224d893d34
gunyu1019 <gunyu1019@gmail.com>
2023-12-31 03:18:35 +0900

    [Feat] #1 - Add list_of_posts method (WIP)

--- routers/post_list.py
+++ routers/post_list.py
@@ -1,11 +1,22 @@
 from fastapi import APIRouter
 
+from config.config import get_config
+from models.posts import Posts
+from modules.notion.client import NotionClient
+
+parser = get_config()
 router = APIRouter()
+database_id = parser.get("Notion", "database")
+client = NotionClient(
+    api_key=parser.get("Notion", "api_key"), version=parser.get("Notion", "version")
+)
 
 
 @router.get("/posts/")
 async def list_of_posts():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+    pages = await client.query_database(database_id=database_id)
+    posts = [[y.text for y in x.title] for x in pages]
+    return posts
 
 
 def setup(app):
