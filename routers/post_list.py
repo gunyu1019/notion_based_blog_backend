d435a742d2a5e5c8433574d328ef7e5e29293528
gunyu1019 <gunyu1019@gmail.com>
2024-01-14 02:31:27 +0900

    [Feat] Complete making a method called 'list_of_posts' for reading list of posts

--- routers/post_list.py
+++ routers/post_list.py
@@ -1,22 +1,26 @@
 from fastapi import APIRouter
 
 from config.config import get_config
-from models.posts import Posts
+from models.post_item import PostItem
 from modules.notion.client import NotionClient
 
 parser = get_config()
 router = APIRouter()
 database_id = parser.get("Notion", "database")
-client = NotionClient(
-    api_key=parser.get("Notion", "api_key"), version=parser.get("Notion", "version")
-)
 
 
 @router.get("/posts/")
-async def list_of_posts():
+async def list_of_posts(private_access: bool = False):
+    client = NotionClient(
+        api_key=parser.get("Notion", "api_key"), version=parser.get("Notion", "version")
+    )
     pages = await client.query_database(database_id=database_id)
-    posts = [[y.text for y in x.title] for x in pages]
-    return posts
+    await client.close()
+    return [
+        PostItem.from_notion(x)
+        for x in pages
+        if not private_access or x.property("published") is True
+    ]
 
 
 def setup(app):
