f7d65d022d0b1748c7faed1aa4401976a605f02b
gunyu1019 <gunyu1019@gmail.com>
2024-01-21 03:31:17 +0900

    [Fix] Add `parent_id` column for Block.

--- models/database/rich_text.py
+++ models/database/rich_text.py
@@ -1,15 +1,19 @@
 from sqlalchemy import String, ForeignKey, Enum
 from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.types import Text
 from typing import TYPE_CHECKING
 
 from models.database.base import Base
+
+if TYPE_CHECKING:
+    from models.database.block import Block
 
 
 class RichText(Base):
     __tablename__ = "rich_text"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    text: Mapped[str] = mapped_column(String(65535), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
 
     # Annotated
     bold: Mapped[bool] = mapped_column(default=False)
@@ -20,4 +24,6 @@ class RichText(Base):
     # Text Option
     color: Mapped[str] = mapped_column(String(10))
     backgroundColorable: Mapped[bool] = mapped_column(default=False)
-    href: Mapped[str] = mapped_column(String(65535), nullable=True, default=None)
+    href: Mapped[str] = mapped_column(Text, nullable=True, default=None)
+
+    parent_id: Mapped[str] = mapped_column(ForeignKey("block.id"))
