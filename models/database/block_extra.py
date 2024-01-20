dc2fbfcd53e56f1f92b32a166687354ba83665bc
gunyu1019 <gunyu1019@gmail.com>
2024-01-21 03:29:50 +0900

    [Feat] Add block extra model (ex. embed's url, equation, table's width...)

---
+++ models/database/block_extra.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.types import Text
+
+from models.database.base import Base
+
+if TYPE_CHECKING:
+    from models.database.block import Block
+
+
+class BlockExtra(Base):
+    __tablename__ = "block_extra"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    type: Mapped[str] = mapped_column(String(10), nullable=False)
+    data: Mapped[str] = mapped_column(Text, nullable=False)
+
+    parent: Mapped["Block"] = relationship(back_populates="extra")
+    parent_id: Mapped[str] = mapped_column(ForeignKey("block.id"))
