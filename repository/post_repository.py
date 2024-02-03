362745efd2c9e54b3edeb2f666f6e3af1206fc3e
gunyu1019 <gunyu1019@gmail.com>
2024-02-04 03:43:27 +0900

    [Feat] Add get_block method

--- repository/post_repository.py
+++ repository/post_repository.py
@@ -1,12 +1,35 @@
-import datetime
-from typing import Sequence
-
+from sqlalchemy.orm import (
+    selectinload,
+    subqueryload,
+    lazyload,
+    immediateload,
+    joinedload,
+    selectin_polymorphic,
+)
 from sqlalchemy.sql import select
 
+from models.database.page import Page
+from models.database.block import Block
+from models.database.block_extra import BlockExtra
+from models.database.rich_text import RichText
 from repository.base_repository import BaseRepository
 
 
 class PostRepository(BaseRepository):
+    async def get_block(self, page_id: str) -> Page:
+        query = (
+            select(Page)
+            .where(Page.id == page_id)
+            .options(
+                selectinload(Page.blocks),
+                selectinload(Page.blocks).selectinload(
+                    Block.children, recursion_depth=100
+                ),
+            )
+        )
+        result = await self._session.execute(query)
+        return result.scalar_one()
+
     # async def get_meal(
     #         self,
     #         building: list[Building],
