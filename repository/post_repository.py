f7f4d3d7fe4cfc2cdff37c3aed19455ce51e0917
gunyu1019 <gunyu1019@gmail.com>
2024-02-04 20:22:30 +0900

    [Fix] Add insert, delete and merge method

--- repository/post_repository.py
+++ repository/post_repository.py
@@ -1,13 +1,19 @@
 from sqlalchemy.orm import selectinload
-from sqlalchemy.sql import select
+from sqlalchemy.sql import select, exists
 
+from models.notion.block import BLOCKS
 from models.database.block import Block
 from models.database.page import Page
 from repository.base_repository import BaseRepository
 
 
 class PostRepository(BaseRepository):
-    async def get_block(self, page_id: str) -> Page:
+    async def exist_block(self, page_id: str) -> bool:
+        query = select(exists(Page).where(Page.id == page_id))
+        result = await self._session.execute(query)
+        return result.scalar_one_or_none() or False
+
+    async def get_block(self, page_id: str) -> Page | None:
         query = (
             select(Page)
             .where(Page.id == page_id)
@@ -19,14 +25,29 @@ class PostRepository(BaseRepository):
             )
         )
         result = await self._session.execute(query)
-        return result.scalar_one()
+        return result.scalar_one_or_none()
 
-    # async def get_meal(
-    #         self,
-    #         building: list[Building],
-    #         date: datetime.date
-    # ) -> Sequence[MealInfo]:
-    #     query = select(MealInfo).where(MealInfo.date == date).where(MealInfo.building.in_(building))
-    #     result = await self._session.execute(query)
-    #     return result.scalars().all()
-    pass
+    @staticmethod
+    def page_model_validate(page_id: str, blocks: list[BLOCKS]) -> Page:
+        page_model = Page(id=page_id)
+        page_model.blocks.extend([
+            Block.from_block(x, index=i)
+            for (i, x) in enumerate(blocks)
+        ])
+        return page_model
+
+    async def insert_block(self, page_id: str, blocks: list[BLOCKS]):
+        page_model = self.page_model_validate(page_id, blocks)
+        self._session.add(page_model)
+        await self._session.commit()
+
+    async def delete_block(self, page_id: str) -> bool:
+        origin_block = await self.get_block(page_id)
+        await self._session.delete(origin_block)
+        await self._session.commit()
+        return True
+
+    async def merge_block(self, new_block: Page) -> bool:
+        await self._session.merge(new_block)
+        await self._session.commit()
+        return True
