e11c30101ee3cdd89834adb0524c12f08fe54d65
gunyu1019 <gunyu1019@gmail.com>
2024-02-08 02:13:05 +0900

    [Feat] Add is_file_available attribute at response.Block model

--- models/response/block.py
+++ models/response/block.py
@@ -7,7 +7,10 @@ class Block(BaseModel):
     id: str
     type: str
     text: list[RichText] = Field(default_factory=list)
+
     has_children: bool = Field(default=False)
     children: list["Block"] = Field(default_factory=list)
+
+    is_file_available: bool = Field(default=False)
 
     model_config = ConfigDict(from_attributes=True)
