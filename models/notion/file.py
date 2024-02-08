d4293c6ad297ff78805cbcaf438cbb487f1785fd
gunyu1019 <gunyu1019@gmail.com>
2024-02-09 04:35:08 +0900

    [Feat] Add computed_filed at notion.File

--- models/notion/file.py
+++ models/notion/file.py
@@ -1,6 +1,7 @@
 import datetime
 
 from pydantic import BaseModel, PrivateAttr
+from pydantic.fields import computed_field
 
 
 class File(BaseModel):
@@ -11,10 +12,12 @@ class File(BaseModel):
         super().__init__(**data)
         self._data = data.get(self.type)
 
+    @computed_field
     @property
     def url(self) -> str:
         return self._data["url"]
 
+    @computed_field
     @property
     def expiry_time(self) -> datetime.datetime | None:
         if "expiry_time" not in self._data or self.type == "external":
