666b3aa29a89ea157e7f839b860a4d36655c7e02
gunyu1019 <gunyu1019@gmail.com>
2025-08-17 01:24:27 +0900

    [Fix] Type Issue

--- models/notion/property/date_property.py
+++ models/notion/property/date_property.py
@@ -6,4 +6,4 @@ from pydantic import BaseModel
 class DateProperty(BaseModel):
     start: datetime.datetime
     end: datetime.datetime | None = None
-    time_zone: str = None
+    time_zone: str | None = None
