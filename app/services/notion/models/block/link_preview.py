c8e73806097076679ed093b09600cae8465678bf
gunyu1019 <gunyu1019@gmail.com>
2026-03-01 18:47:58 +0900

    [Feat] Add computed_field decorator to LinkPreview model for url property

--- app/services/notion/models/block/link_preview.py
+++ app/services/notion/models/block/link_preview.py
@@ -1,7 +1,10 @@
+from pydantic import computed_field
+
 from .base_block import BaseBlock
 
 
 class LinkPreview(BaseBlock):
+    @computed_field
     @property
     def url(self) -> str:
         return self._data["url"]
